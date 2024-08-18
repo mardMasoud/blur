f591fc44446697150a741b8a42c39931d1d27004
abc <you@example.com>
2024-08-18 16:30:16 +0330

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [per, setPer] = useState(50);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // const [tar, setTar] = useState(100);
+    useEffect(() => {
+        if (per > 0) {
+        
+            const interval = setInterval(() => setPer(per - 1), 10);
+            return () => {
+                clearInterval(interval);
+            };
+            
+        }
+       
+      
+    }, [per]);
+
+    return (
+        <div>
+            <p className="per" onClick={() => setPer(per + 1)} style={{opacity:`${(per/100)-0.01}`}}>
+                {100-per}%
+            </p>
+            <div className="app" style={{ filter: `blur(${per-1}px)` }}></div>
+        </div>
+    );
 }
 
-export default App
+export default App;
