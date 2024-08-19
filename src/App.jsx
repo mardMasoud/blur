eff628851a7d6f5525d267362ba237866d4b2f68
abc <you@example.com>
2024-08-19 16:42:31 +0330

    v3

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,29 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-    const [per, setPer] = useState(50);
+    const [per, setPer] = useState(100);
 
     // const [tar, setTar] = useState(100);
     useEffect(() => {
-        if (per > 0) {
-        
-            const interval = setInterval(() => setPer(per - 1), 10);
+        console.log(per)
+        if (per > 1) {
+            const interval = setInterval(() => setPer((per) - 1), 50);
             return () => {
                 clearInterval(interval);
             };
-            
         }
-       
-      
-    }, [per]);
+    });
 
     return (
         <div>
-            <p className="per" onClick={() => setPer(per + 1)} style={{opacity:`${(per/100)-0.01}`}}>
-                {100-per}%
+            <p
+                className="per"
+                
+                style={{ opacity: `${per / 100 }` }}
+            >
+                {100 - (per)}%
             </p>
-            <div className="app" style={{ filter: `blur(${per-1}px)` }}></div>
+            <div className="app" style={{ filter: `blur(${(per/10)*3}px)` }}></div>
         </div>
     );
 }
