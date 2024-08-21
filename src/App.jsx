6a3b60542d1f5bea235a95b9bb2cbcee2fcb8324
abc <you@example.com>
2024-08-21 17:11:04 +0330

    v4

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,27 @@ import "./App.css";
 
 function App() {
     const [per, setPer] = useState(100);
+ 
+    // if (per > 1) {
+    //    const  interval = setInterval(() => setPer((per) - 1), 50);
+    // if(per===0)
+    //         clearInterval(interval);
+      
 
     // const [tar, setTar] = useState(100);
-    useEffect(() => {
-        console.log(per)
-        if (per > 1) {
-            const interval = setInterval(() => setPer((per) - 1), 50);
-            return () => {
-                clearInterval(interval);
-            };
-        }
-    });
-
+    // useEffect(() => {
+    //     console.log(per)
+    //     if (per > 1) {
+    //         // const interval = 
+    //         setTimeout(() => setPer((per) - 1), 50);
+    //         // return () => {
+    //         //     clearInterval(interval);
+    //         // };
+    //     }
+    // });
+    if (per > 1) 
+    setTimeout(() => setPer((per) - 1), 10);
+console.log(per)
     return (
         <div>
             <p
